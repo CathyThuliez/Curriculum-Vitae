d36e6a5784a67e7fb3be3621eba161aaf9b52214
CathyThuliez <popschool@debian>
2017-08-02 09:25:08 +0200

    Update

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -8,7 +8,7 @@ function scrollToSection(section) {
   window.scrollTo(0, ancreY - navbarHeight);
  };
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 40px from the top of the document, show the button
 window.onscroll = function() {scrollFunction() };
 
 function scrollFunction() {
